3160f785f5a76b18823677de45248d03cb6ba44b
Julia <julia.masaeva@gmail.com>
2023-01-20 23:13:08 +0600

    последняя цифра 3-х значного числа

---
+++ Task005/Program.cs
@@ -0,0 +1,6 @@
+﻿//Показать последнюю цифру трёхзначного числа
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int a = number % 10;
+Console.WriteLine(a);
+
