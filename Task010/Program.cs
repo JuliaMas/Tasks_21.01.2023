cc5bb5feb0d5586973c2c7da38065777ea5130b7
Julia <julia.masaeva@gmail.com>
2023-01-21 00:56:25 +0600

    Найти третью цифру числа

---
+++ Task010/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+string numberText = Convert.ToString(number);
+if(numberText.Length > 2)
+{
+    Console.WriteLine("Третья цифра = " + numberText[2]);
+}
+else
+{
+    Console.WriteLine("Третья цифра отсутствует");
+}
