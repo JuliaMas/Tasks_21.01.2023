ad8050bae6d841caf2f953cff5f7636e57b5e1a7
Julia <julia.masaeva@gmail.com>
2023-01-21 00:33:20 +0600

    Проветка на кратность

--- Task008/Program.cs
+++ Task008/Program.cs
@@ -1,20 +1,7 @@
 ﻿// Удалить вторую цифру трёхзначного числа
-//Console.Write("Введите трехзначное число: ");
-//int number = int.Parse(Console.ReadLine());
-//int a = (number / 100)*10;
-//int c = number % 10;
-//int N = a + c;
-//Console.WriteLine(N);
-
-Console.WriteLine("Введите число");
-int x = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите позицию");
-int s = int.Parse(Console.ReadLine());
-int m = 1;
-while (s > 1) 
-{
-     m *= 10;
-     s--;
-}
-int t = (x % m) + (x / (m * 10)) * m;
-Console.WriteLine(t);
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int a = (number / 100)*10;
+int c = number % 10;
+int N = a + c;
+Console.WriteLine(N);
---
+++ Task009/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Введите число A =  "); // Заданное число
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B = ");  
+int B = int.Parse(Console.ReadLine());
+int n = A % B;
+if(A % B == 0)
+{
+    Console.WriteLine("Число кратно");
+}
+else
+{
+    Console.WriteLine("Число не кратно, остаток равен " + n);
+}
