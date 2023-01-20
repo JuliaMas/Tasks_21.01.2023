1ae399a1ac3a9db5670624a10fd495c6997202b7
Julia <julia.masaeva@gmail.com>
2023-01-20 23:36:12 +0600

    Показать наибольшую цифру 2-х значного числа

---
+++ Task007/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int a = N / 10;
+int b = N % 10;
+if(a > b)
+{
+    Console.WriteLine(a);
+}
+else 
+{
+    Console.WriteLine(b);
+}
