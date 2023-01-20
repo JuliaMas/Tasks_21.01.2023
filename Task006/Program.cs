e764674301264afca9031be791409a6f0c1869a7
Julia <julia.masaeva@gmail.com>
2023-01-20 23:18:20 +0600

    вторая цифра 3-х значного числа

---
+++ Task006/Program.cs
@@ -0,0 +1,5 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int a = (number/10) % 10;
+Console.WriteLine(a);
